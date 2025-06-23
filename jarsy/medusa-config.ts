1e665a7841a8d795f66cb8cf09827e28af7446b4
Gyanender Singh <gyanendersingh5@gmail.com>
2025-06-23 19:36:08 +0530

    added backendurl

--- jarsy/medusa-config.ts
+++ jarsy/medusa-config.ts
@@ -1,6 +1,7 @@
 import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
+const backendUrl = process.env.MEDUSA_BACKEND_URL || "http://localhost:9000";
 
 module.exports = defineConfig({
   projectConfig: {
@@ -14,12 +15,43 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
-  modules: {
-    eventBus: {
-      resolve: "@medusajs/event-bus-redis",
+  admin: {
+    backendUrl,
+  },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/cache-redis",
       options: {
         redisUrl: process.env.REDIS_URL,
       },
     },
-  },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-local",
+            options: {
+              uploadDir: "static",
+              backend_url: `${backendUrl}/static`,
+            },
+          },
+        ],
+      },
+    },
+  ],
 });
